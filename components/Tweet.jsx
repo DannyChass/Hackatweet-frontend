3434ac2e75aaaf2820b58617d63eaf624042fbaf
Daniel Chassagnez Goncalves <daniel.chassagn@gmail.com>
2025-11-20 16:15:25 +0100

    add Tweet component

---
+++ components/Tweet.jsx
@@ -0,0 +1,45 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from 'react';
+import styles from '../styles/Tweet.module.css';
+
+function Tweet({ tweet, onDelete }) {
+
+    const user = useSelector((state) => state.user.value[0]);
+    const [liked, setLiked] = useState(false);
+
+    const timeSince = (date) => {
+        const now = new Date();
+        const diff = Math.floor((now - new Date(date)) / 3600000);
+        return diff === 0 ? "Just now" : `${diff} hour${diff > 1 ? "s" : ""} ago`;
+    };
+
+    const toggleLike = () => {
+        setLike(!liked);
+    };
+
+    return (
+        <div className={styles.tweetContainer}>
+            <div className={styles.header}>
+                <span className={styles.name}>{tweet.author.firstname}</span>
+                <span className={styles.username}>@{tweet.author.username}</span>
+                <span className={styles.time}>{timeSince(tweet.createdAt)}</span>
+            </div>
+
+            <div className={styles.content}>{tweet.content}</div>
+
+            <div className={styles.actions}>
+                <button className={styles.like} onClick={toggleLike}>
+                    {liked ? "❤️" : "🤍"}
+                </button>
+
+                {user && user.username === tweet.author.username && (
+                    <button className={styles.delete} onClick={() => onDelete(tweet._id)}>
+                        🗑️
+                    </button>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Tweet;
