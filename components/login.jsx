c5e74ead2601be6e6f08fed766080e10cd35759c
Sylvain <sylvain.berthere@gmail.com>
2025-11-20 12:36:15 +0100

    front hacka with register

--- components/login.jsx
+++ components/login.jsx
@@ -1,21 +1,47 @@
+import { useState } from 'react'
 import styles from '../styles/Login.module.css'
 
 
 function Login() {
 
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [firstname, setFirstname] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
     const showModal = () => {
-        return (
-            <>
-                <div className={styles.modal} >
-                    <input type='text' placeholder='firstname' />
-                    <input type='text' placeholder='username' />
-                    <input type='text' placeholder='password' />
-                </div>
-            </>
-        )
+        setIsModalVisible(true);
+    }
+
+    const hideModal = () => {
+        setIsModalVisible(false);
+    }
+
+    const addUser = () => {
+        fetch('http://localhost:3000/users/signup', {
+            method : 'POST',
+            headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({firstname : firstname, username: username, password: password }),
+        })
+        .then (response => response.json())
+        .then (data => {
+            console.log("New User add")
+            console.log(data)
+        })
+
     }
 
 
+    const modal = (
+        <>
+            <div className={styles.modal} >
+                <input onChange={(e) => setFirstname(e.target.value)} value={firstname} className={styles.input} type='text' placeholder='firstname' />
+                <input onChange={(e) => setUsername(e.target.value)} value={username} className={styles.input} type='text' placeholder='username' />
+                <input onChange={(e) => setPassword(e.target.value)} value={password} className={styles.input} type='text' placeholder='password' />
+                <button onClick={() => addUser()} className={styles.buttonSignUp}>Register</button>
+            </div>
+        </>
+    );
 
     return (
         <div>
@@ -28,12 +54,15 @@ function Login() {
                     <h1 className={styles.title1}>See what's happening</h1>
                     <h4 className={styles.title2}>Join Hackatweet today.</h4>
                     <button onClick={() => showModal()} className={styles.buttonSignUp}>Sign Up</button>
+                    {isModalVisible && modal}
                     <span className={styles.text}>Already have an account ?</span>
-                    <button onClick={() => showModal()} className={styles.buttonSignIn}>Sign In</button>
+                    <button className={styles.buttonSignIn}>Sign In</button>
 
                 </div>
 
             </div>
+
+            
 
         </div>
     )
