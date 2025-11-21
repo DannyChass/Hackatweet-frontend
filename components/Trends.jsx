2fda34631a2a6cfde1dce65f60b8b13144f5bdce
Daniel Chassagnez Goncalves <daniel.chassagn@gmail.com>
2025-11-21 12:30:22 +0100

    fix Trends

--- components/Trends.jsx
+++ components/Trends.jsx
@@ -5,23 +5,26 @@ import styles from '../styles/Trends.module.css';
 function Trends() {
     const [trends, setTrends] = useState([]);
 
-    useEffect(async () => {
-        const response = await fetch("http://localhost:3000/trends/all");
+    useEffect(() => {
+        async function loadTrends() {
+            const res = await fetch("http://localhost:3000/trends/all");
+            const data = await res.json();
 
-        const data = await response.json();
-
-        if (data.result) {
-            setTrends(data.trends);
+            if (data.result) {
+                setTrends(data.trends);
+            }
         }
+
+        loadTrends();
     }, []);
 
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Trends</h1>
 
-            {trends.map((t, i) => {
-                <Trend key={i} name={t.name} count={t.tweetCount}></Trend>
-            })}
+            {trends.map((t, i) => (
+                <Trend key={i} name={t.name} count={t.tweetCount} />
+            ))}
         </div>
     )
 }
